edf3706b966362cb3691ac741eaa5163b2e1a138
Gabriel Valle <gvalle91@gmail.com>
2018-06-13 20:26:42 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,8 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  puts batch_badge_creator(array)
-  puts assign_rooms(array)
+  array.each do |name|
+    puts batch_badge_creator(name)
+    puts assign_rooms(name)
+  end
 end
