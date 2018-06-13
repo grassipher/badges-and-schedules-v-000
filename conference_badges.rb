5d9954d3aeb74544cb68c78705ec2538793ea79a
Gabriel Valle <gvalle91@gmail.com>
2018-06-13 19:58:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,12 @@
 # Write your code here.
-def badge_maker
-  
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array.push(badge_maker(name))
+  end
+  return new_array
 end
